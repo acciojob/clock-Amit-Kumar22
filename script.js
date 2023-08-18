d2eadeed85638d3ea1febdbc8b088c17fc0ffe75
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-08-18 12:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const d = new Date();
+document.getElementById("demo").innerHTML = d.getMonth() + 1+"/"+d.getDate()+"/"+
+	d.getYear()+", "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+" PM";
